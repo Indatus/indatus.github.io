1d20201733972e95f72098cbe5945704682aef04
Brian Webb <bwebb@indatus.com>
2014-05-13 10:26:03 -0400

    updated build script

--- build/indatus-github-io-html-split.sh
+++ build/indatus-github-io-html-split.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf converted-html/build
 git subsplit init git@github.com:indatus/indatus.github.io-source.git
 git subsplit publish --heads="master" --no-tags converted-html:git@github.com:indatus/indatus.github.io
 rm -rf .subsplit/
